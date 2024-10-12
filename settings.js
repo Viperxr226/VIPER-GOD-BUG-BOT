eca6907e4705ed0ab62d6d412a876b7b42c596c6
Viperxr226 <uzezijenniferadeniyi@gmail.com>
2024-10-12 16:53:14 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,25 +4,25 @@
 //Instagram: techgod143
 //Telegram: t.me/techgod143
 //GitHub: @techgod143
-//WhatsApp: +917466008456
+//WhatsApp: +2348022142390
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@techgod143
 const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "2348022142390"
+global.ownername = "🐛Viper God"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
-global.location = "India, Up, Rampur"
+global.location = "Nigeria, Delta, Oghara"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '2338022142390'  //creator number
+global.ownername = '🐛Viper Lord' //owner name
+global.botname = 'Viper God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'Tech-God\n\nContact: +917466008456'
+global.author = 'Tech-God\n\nContact: +2348022142390'
 
 //console view/theme
 global.themeemoji = '🪀'
